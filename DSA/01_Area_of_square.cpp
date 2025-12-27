59ce1bc82042baff88af700bd48d0d937dc62047
Montukharb <montukharb868@gmail.com>
2025-12-27 11:24:10 +0530

    update code

--- DSA/01_Area_of_square.cpp
+++ DSA/01_Area_of_square.cpp
@@ -1,11 +1,25 @@
 #include <iostream>
-using namespace std;
-
+// using namespace std;
+void display(int side)
+{
+    std::cout<<"Area of square is = "<<side*side; //Multiply side * side and print result
+    
+}
+void Squareparameter(int side);
 int main()
 {
    int side = 0;
-    cout<<"Enter lenght of any one side of square\n";
-    cin>>side;
-    cout<<"Area of square is = "<<side*side; //Multiply side * side and print result
+    std::cout<<"Enter lenght of any one side of square\n";
+    std::cin>>side;
+    display(side);
+    Squareparameter(side);
     return 0;
 }
+
+void Squareparameter(int side = 2)
+{
+    float p_square = 0;
+    p_square = ( 4 * side);
+    std::cout<<"\tParameter of Square is : "<<p_square;
+}
+
